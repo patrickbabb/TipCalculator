2b26e6859b274616f08ea4973a046c4ee416836d
Patrick Babb <patrick.r.babb@gmail.com>
2024-03-18 20:45:27 -0500

    Add project files.

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void etnEnterBillAmount_Completed(object sender, EventArgs e)
+        {
+            lblGrandTotal.Text = "$" + etnEnterBillAmount.Text;
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var totalWithTip = decimal.Parse(etnEnterBillAmount.Text) * 15;
+
+            lblGrandTotal.Text = "$" + totalWithTip.ToString();
+        }
+    }
+}
