284277b69b5ceb2a95b7b3f96cad9a91a2156dc1
Patrick Babb <patrick.r.babb@gmail.com>
2024-03-19 23:01:17 -0500

    -Added functionality to tip percentages -Fields in the totals frame update on click

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 {
     public partial class MainPage : ContentPage
     {
+        decimal subtotal;
+
         public MainPage()
         {
             InitializeComponent();
@@ -10,14 +12,22 @@
         private void etnEnterBillAmount_Completed(object sender, EventArgs e)
         {
             lblGrandTotal.Text = "$" + etnEnterBillAmount.Text;
+            subtotal = decimal.Parse(etnEnterBillAmount.Text);
+            lblSubtotal.Text = subtotal.ToString();
 
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            var totalWithTip = decimal.Parse(etnEnterBillAmount.Text) * 15;
+            var tip = (decimal.Parse(etnEnterBillAmount.Text) / 100) * 15;
+            
+            var totalWithTip = subtotal + tip;
+
+            totalWithTip = Math.Round(totalWithTip, 2);
 
             lblGrandTotal.Text = "$" + totalWithTip.ToString();
+            lblTip.Text = tip.ToString();
+            
         }
     }
 }
