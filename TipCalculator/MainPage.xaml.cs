f4f8b5a11d6899ceaa0220d8076ac0738b443e6e
Patrick Babb <patrick.r.babb@gmail.com>
2024-03-20 21:20:20 -0500

    -Added functionality to all tip buttons -Added functionality to tip slider -Added functionality to party size selector

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -3,6 +3,9 @@
     public partial class MainPage : ContentPage
     {
         decimal subtotal;
+        decimal tip;
+        decimal totalWithTip;
+        int partySize = 0;
 
         public MainPage()
         {
@@ -11,23 +14,65 @@
 
         private void etnEnterBillAmount_Completed(object sender, EventArgs e)
         {
-            lblGrandTotal.Text = "$" + etnEnterBillAmount.Text;
             subtotal = decimal.Parse(etnEnterBillAmount.Text);
-            lblSubtotal.Text = subtotal.ToString();
 
+            lblSubtotal.Text = "$" + etnEnterBillAmount.Text;
+
+            lblGrandTotal.Text = "TEST";
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private void Button_Clicked15(object sender, EventArgs e)
         {
-            var tip = (decimal.Parse(etnEnterBillAmount.Text) / 100) * 15;
-            
-            var totalWithTip = subtotal + tip;
+            Calculate(15);
+        }
 
+        private void Button_Clicked20(object sender, EventArgs e)
+        {
+            Calculate(20);
+        }
+
+        private void Button_Clicked25(object sender, EventArgs e)
+        {
+            Calculate(25);
+        }
+
+        private void Calculate(int tipPercent)
+        {
+            tip = (decimal.Parse(etnEnterBillAmount.Text) / 100) * tipPercent;
+            totalWithTip = subtotal + tip;
             totalWithTip = Math.Round(totalWithTip, 2);
 
-            lblGrandTotal.Text = "$" + totalWithTip.ToString();
+            lblSubtotal.Text = "$" + totalWithTip.ToString();
             lblTip.Text = tip.ToString();
+        }
+
+        private void sldTipPercent_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var sliderTip = sldTipPercent.Value;
+            lblTipSlider.Text = Math.Round(sliderTip).ToString() + "%";
+
+            Calculate((int)sliderTip);
+        }
+
+        private void btnRemovePerson_Clicked(object sender, EventArgs e)
+        {
+            if (partySize <= 0)
+            {
+                partySize = 0;
+            }
+            else
+            {
+                partySize--;
+            }
             
+            lblPartySize.Text = partySize.ToString();
+        }
+
+        private void btnAddPerson_Clicked(object sender, EventArgs e)
+        {
+            partySize++;
+
+            lblPartySize.Text = partySize.ToString();
         }
     }
 }
