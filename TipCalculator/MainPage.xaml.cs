81f5ab8ac1fc43b34287eb1a8f32830960dd71a1
Patrick Babb <patrick.r.babb@gmail.com>
2024-03-24 18:55:04 -0500

    All buttons now work with one another. The Calculate() method is now the central logic.

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -3,9 +3,11 @@
     public partial class MainPage : ContentPage
     {
         decimal subtotal;
-        decimal tip;
+        decimal tip = 0;
         decimal totalWithTip;
-        int partySize = 0;
+        int partySize = 1;
+        int tipPercent = 0;
+        double sliderTip;
 
         public MainPage()
         {
@@ -18,54 +20,28 @@
 
             lblSubtotal.Text = "$" + etnEnterBillAmount.Text;
 
-            lblGrandTotal.Text = "TEST";
-        }
-
-        private void Button_Clicked15(object sender, EventArgs e)
-        {
-            Calculate(15);
-        }
-
-        private void Button_Clicked20(object sender, EventArgs e)
-        {
-            Calculate(20);
-        }
-
-        private void Button_Clicked25(object sender, EventArgs e)
-        {
-            Calculate(25);
-        }
-
-        private void Calculate(int tipPercent)
-        {
-            tip = (decimal.Parse(etnEnterBillAmount.Text) / 100) * tipPercent;
-            totalWithTip = subtotal + tip;
-            totalWithTip = Math.Round(totalWithTip, 2);
-
-            lblSubtotal.Text = "$" + totalWithTip.ToString();
-            lblTip.Text = tip.ToString();
+            Calculate();
         }
 
         private void sldTipPercent_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            var sliderTip = sldTipPercent.Value;
-            lblTipSlider.Text = Math.Round(sliderTip).ToString() + "%";
-
-            Calculate((int)sliderTip);
+            Calculate();
         }
 
         private void btnRemovePerson_Clicked(object sender, EventArgs e)
         {
-            if (partySize <= 0)
+            if (partySize <= 1)
             {
-                partySize = 0;
+                partySize = 1;
             }
             else
             {
                 partySize--;
             }
-            
+
             lblPartySize.Text = partySize.ToString();
+
+            Calculate();
         }
 
         private void btnAddPerson_Clicked(object sender, EventArgs e)
@@ -73,6 +49,43 @@
             partySize++;
 
             lblPartySize.Text = partySize.ToString();
+
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            tip = (decimal.Parse(etnEnterBillAmount.Text) / 100) * tipPercent;
+            totalWithTip = subtotal + tip;
+            totalWithTip = Math.Round(totalWithTip, 2) / partySize;
+            totalWithTip = Math.Round(totalWithTip, 2);
+
+            sliderTip = sldTipPercent.Value;
+            lblTipSlider.Text = Math.Round(sliderTip).ToString() + "%";
+            tipPercent = (int)sliderTip;
+
+            lblSubtotal.Text = "$" + totalWithTip.ToString();
+            lblTip.Text = "$" + Math.Round(tip, 2).ToString();
+
+            lblGrandTotal.Text = "$" + totalWithTip.ToString();
+        }
+
+        private void Button_Clicked15(object sender, EventArgs e)
+        {
+            tipPercent = 15;
+            Calculate();
+        }
+
+        private void Button_Clicked20(object sender, EventArgs e)
+        {
+            tipPercent = 20;
+            Calculate();
+        }
+
+        private void Button_Clicked25(object sender, EventArgs e)
+        {
+            tipPercent = 25;
+            Calculate();
         }
     }
 }
