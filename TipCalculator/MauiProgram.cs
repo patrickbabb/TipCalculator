ecadcfd370ff38d7aba0b7f9c679c8d2b7049bb3
Patrick Babb <patrick.r.babb@gmail.com>
2024-03-25 00:11:33 -0500

    The theme added and status bar color changed.

--- TipCalculator/MauiProgram.cs
+++ TipCalculator/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace TipCalculator
 {
@@ -9,11 +10,14 @@ namespace TipCalculator
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+
 
 #if DEBUG
     		builder.Logging.AddDebug();
